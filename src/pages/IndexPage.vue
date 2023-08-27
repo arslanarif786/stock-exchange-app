41eedd9a42aa1ffa84bd8428ff1f9ff4f1ead070
Muhammad Arslan <m.arslan.0011333@gmail.com>
2023-08-27 21:13:42 +0500

    feat: project setup created

---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,24 @@
+<template>
+  <q-page class="flex flex-center">
+    <div class="">
+      <div class="text-red-400"> Vue installed</div>
+      <div class="text-green-400"> Quasar installed</div>
+      <div class="text-blue-400"> Composition API done</div>
+      <div class="text-green-400">
+        Tailwind installed
+      </div>
+      <div class="text-blue-400"> pinia installed</div>
+      <div class="text-red-400"> composables created</div>
+      </div>
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+import { useSample } from '../composables/useSample.js'
+// const { x, y } = useSample()
+
+export default defineComponent({
+  name: 'IndexPage'
+})
+</script>
