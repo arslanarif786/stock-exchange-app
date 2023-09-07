ab82ae39ad0e6ae1e6c95cdf8b7dc841f327917d
Muhammad Arslan <m.arslan.0011333@gmail.com>
2023-09-08 01:24:06 +0500

    feat: list is now draggable and moveable

--- src/layouts/Sidebar.vue
+++ src/layouts/Sidebar.vue
@@ -16,7 +16,7 @@
         class="bg-white"
         style="height: 91.5vh; max-height: 200vh; width: 431px"
       >
-      <!-- Search Bar -->
+      <!------------------------------- Search Bar ------------------------------->
         <div class="pt-1 text-center">
           <q-input
             v-model="searchQuery"
@@ -35,11 +35,10 @@
           <q-separator class="bg-gray-200"/>
         </div>
 
-        <!-- Search List -->
-        <SearchList
-          v-for="item in filteredList"
-          :key="item.title"
-          v-bind="item"
+        <!---------------------------- Draggable List ---------------------------->
+        <draggable-list
+          :search-query="searchQuery"
+          @list-count="getListCount"
           />
       </div>
     </div>
@@ -47,75 +46,14 @@
 </template>
 
 <script setup>
-import { computed, ref } from "vue"
-import SearchList from "components/search/SearchList.vue"
+import { ref } from "vue"
+import DraggableList from "../components/draggable/DraggableList.vue"
+
 const leftDrawerOpen = ref(false)
+const listCount = ref(0)
 const searchQuery = ref("")
-const lists = ref([
-  {
-    title: "SUZLON",
-    caption: "",
-    changeRate: -0.55,
-    percentage: 6.99,
-    changedPrice: 33.75,
-  },
-  {
-    title: "ZEAL",
-    caption: "BSE",
-    changeRate: -0.08,
-    percentage: 4.89,
-    changedPrice: 18.06,
-  },
-  {
-    title: "TAPARIA",
-    caption: "BSE",
-    changeRate: "0.00",
-    percentage: "0.00",
-    changedPrice: 2.66,
-  },
-  {
-    title: "VISHAL",
-    caption: "",
-    changeRate: 1.25,
-    percentage: 3.54,
-    changedPrice: 24.65,
-  },
-  {
-    title: "JUMBO",
-    caption: "BSE",
-    changeRate: 0.78,
-    percentage: 4.89,
-    changedPrice: 6.09,
-  },
-  {
-    title: "RANASUG",
-    caption: "",
-    changeRate: -0.05,
-    percentage: 3.22,
-    changedPrice: 24.65,
-  },
-  {
-    title: "UNISON",
-    caption: "BSE",
-    changeRate: -0.33,
-    percentage: 5.15,
-    changedPrice: 24.65,
-  },
-  {
-    title: "KEIL",
-    caption: "BSE",
-    changeRate: 0.75,
-    percentage: 4.77,
-    changedPrice: 21.66,
-  },
-])
 
-const filteredList = computed(() => {
-  if(typeof searchQuery.value !== "string") {
-    return lists.value
-  }
-  return lists.value.filter((row) => row.title.toLowerCase().includes(searchQuery.value))
-})
-
-const listCount = computed(() => { return filteredList.value.length })
+const getListCount = (count) => {
+  listCount.value = count
+}
 </script>
